21f9f285a449176e5980c0a2822d8f32779268a4
Lodewijk Evers <lodewijk@twokings.nl>
2016-05-18 16:54:53 +0200

    add omnipay/mollie dependency and run composer update --no-dev

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,10 +23,6 @@ class ComposerAutoloaderInitc9ef76b3c4ca80d184aa0c79f412a58f
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitc9ef76b3c4ca80d184aa0c79f412a58f', 'loadClassLoader'));
 
-        $includePaths = require __DIR__ . '/include_paths.php';
-        array_push($includePaths, get_include_path());
-        set_include_path(join(PATH_SEPARATOR, $includePaths));
-
         $map = require __DIR__ . '/autoload_namespaces.php';
         foreach ($map as $namespace => $path) {
             $loader->set($namespace, $path);
